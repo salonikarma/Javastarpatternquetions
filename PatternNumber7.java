344cc6c9628b115aa1c43ff54f512d6a91fa6157
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-08-27 01:42:09 +0530

    Add files via upload

---
+++ PatternNumber7.java
@@ -0,0 +1,16 @@
+public class PatternNumber7 {
+    public static void main(String args[]){
+        int n = 10;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || i==(n-1)/2 || i==n-1 || j==0){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
