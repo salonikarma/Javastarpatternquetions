4e9d98a416f4a4f9ee8607e4c1246c7395e2d810
salonikarma <101549960+salonikarma@users.noreply.github.com>
2023-09-01 22:44:44 +0530

    Add files via upload

---
+++ PatternNumber10.java
@@ -0,0 +1,16 @@
+public class PatternNumber10 {
+    public static void main(String args[]){
+        int n = 10;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
